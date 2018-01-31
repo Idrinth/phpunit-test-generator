8d0f211f1ac8f627c5c6b88e437eaf59b7b420d6
Björn Büttner <Idrinth@users.noreply.github.com>
2018-01-31 22:34:39 +0100

    no more string concat in statics...

--- test/DocBlockParserTest.php
+++ test/DocBlockParserTest.php
@@ -7,34 +7,34 @@ use PHPUnit\Framework\TestCase;
 
 class DocBlockParserTest extends TestCase
 {
-    private static $case1 = "/**\n* @return string \n*/";
-    private static $case2 = "/**\n*@throws \Exception \n"
-        . "* @param \$a\n"
-        . "*@return string \n"
-        . "* @return void\n */";
-    private static $case3 = "/**\n * @param int \$z \n"
-        . "*@return A|B \n*/";
-    private static $case4 = "/**\n*@throws IException \n"
-        . "*@throws UException \n"
-        . "*@param int \$z\n"
-        . "*@param \$m\n"
-        . " @return string \n*/";
-    private static $case5 = "/**\n*@param boolean \$z\n"
-        . "*@param \$m Just a bit of text\n"
-        . " @return void \n*/";
+    /**
+     * @return string[]
+     */
+    private function getCases()
+    {
+        return array(
+            '',
+            "/**\n* @return string \n*/",
+            "/**\n*@throws \Exception \n* @param \$a\n*@return string \n* @return void\n */",
+            "/**\n * @param int \$z \n*@return A|B \n*/",
+            "/**\n*@throws IException \n*@throws UException \n*@param int \$z\n*@param \$m\n @return string \n*/",
+            "/**\n*@param boolean \$z\n*@param \$m Just a bit of text\n @return void \n*/"
+        );
+    }
 
     /**
      * @test
      */
     public function testGetReturn()
     {
+        $cases = $this->getCases();
         $instance = new DocBlockParser();
-        $this->assertEquals('null', $instance->getReturn(''));
-        $this->assertEquals('string', $instance->getReturn(self::$case1));
-        $this->assertEquals('string|void', $instance->getReturn(self::$case2));
-        $this->assertEquals('A|B', $instance->getReturn(self::$case3));
-        $this->assertEquals('string', $instance->getReturn(self::$case4));
-        $this->assertEquals('void', $instance->getReturn(self::$case5));
+        $this->assertEquals('null', $instance->getReturn($cases[0]));
+        $this->assertEquals('string', $instance->getReturn($cases[1]));
+        $this->assertEquals('string|void', $instance->getReturn($cases[2]));
+        $this->assertEquals('A|B', $instance->getReturn($cases[3]));
+        $this->assertEquals('string', $instance->getReturn($cases[4]));
+        $this->assertEquals('void', $instance->getReturn($cases[5]));
     }
 
     /**
@@ -42,17 +42,18 @@ class DocBlockParserTest extends TestCase
      */
     public function testGetParams()
     {
+        $cases = $this->getCases();
         $instance = new DocBlockParser();
         $this->assertCount(0, $instance->getParams(''));
-        $this->assertCount(0, $instance->getParams(self::$case1));
-        $this->assertCount(1, $instance->getParams(self::$case2));
-        $this->assertEquals(array('string'), $instance->getParams(self::$case2));
-        $this->assertCount(1, $instance->getParams(self::$case3));
-        $this->assertEquals(array('int'), $instance->getParams(self::$case3));
-        $this->assertCount(2, $instance->getParams(self::$case4));
-        $this->assertEquals(array('int', 'string'), $instance->getParams(self::$case4));
-        $this->assertCount(2, $instance->getParams(self::$case5));
-        $this->assertEquals(array('boolean', 'string'), $instance->getParams(self::$case5));
+        $this->assertCount(0, $instance->getParams($cases[1]));
+        $this->assertCount(1, $instance->getParams($cases[2]));
+        $this->assertEquals(array('string'), $instance->getParams($cases[2]));
+        $this->assertCount(1, $instance->getParams($cases[3]));
+        $this->assertEquals(array('int'), $instance->getParams($cases[3]));
+        $this->assertCount(2, $instance->getParams($cases[4]));
+        $this->assertEquals(array('int', 'string'), $instance->getParams($cases[4]));
+        $this->assertCount(2, $instance->getParams($cases[5]));
+        $this->assertEquals(array('boolean', 'string'), $instance->getParams($cases[5]));
     }
 
     /**
@@ -60,14 +61,15 @@ class DocBlockParserTest extends TestCase
      */
     public function testGetExceptions()
     {
+        $cases = $this->getCases();
         $instance = new DocBlockParser();
         $this->assertCount(0, $instance->getExceptions(''));
-        $this->assertCount(0, $instance->getExceptions(self::$case1));
-        $this->assertCount(1, $instance->getExceptions(self::$case2));
-        $this->assertEquals(array('\Exception'), $instance->getExceptions(self::$case2));
-        $this->assertCount(0, $instance->getExceptions(self::$case3));
-        $this->assertCount(2, $instance->getExceptions(self::$case4));
-        $this->assertEquals(array('IException', 'UException'), $instance->getExceptions(self::$case4));
-        $this->assertCount(0, $instance->getExceptions(self::$case5));
+        $this->assertCount(0, $instance->getExceptions($cases[1]));
+        $this->assertCount(1, $instance->getExceptions($cases[2]));
+        $this->assertEquals(array('\Exception'), $instance->getExceptions($cases[2]));
+        $this->assertCount(0, $instance->getExceptions($cases[3]));
+        $this->assertCount(2, $instance->getExceptions($cases[4]));
+        $this->assertEquals(array('IException', 'UException'), $instance->getExceptions($cases[4]));
+        $this->assertCount(0, $instance->getExceptions($cases[5]));
     }
 }
