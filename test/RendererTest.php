28e8a7e8eb85f63fdf2c4049629fbc191f77f46a
Björn Büttner <Idrinth@users.noreply.github.com>
2018-03-18 00:15:05 +0100

    psr2 fixes

--- test/RendererTest.php
+++ test/RendererTest.php
@@ -16,12 +16,19 @@ class RendererTest extends TestCase
         return array(
             array(
                 'class',
-                "<?php\n\nnamespace ;\n\nuse  as TestCaseImplementation;\nuse \;\n\n/**\n * this is an automatically generated skeleton for testing \n * @todo actually test\n **/\nclass Test extends TestCaseImplementation\n{\n    /**\n     * @return \n     * @todo make sure the construction works as expected\n     **/\n    protected function getInstance()\n    {\n        return new ();\n    }\n}\n"),
+                "<?php\n\nnamespace ;\n\nuse  as TestCaseImplementation;\nuse \;\n\n/**\n"
+                . " * this is an automatically generated skeleton for testing \n "
+                . "* @todo actually test\n **/\nclass Test extends TestCaseImplementation\n{"
+                . "\n    /**\n     * @return \n     * @todo make sure the construction works"
+                . " as expected\n     **/\n    protected function getInstance()\n    {"
+                . "\n        return new ();\n    }\n}\n"
+            ),
             array('function', '()'),
             array('test-functions', ""),
             array(
                 'type-tests',
-                "\n        \$this->assertInternalType(\n            '',\n            ,\n            'Return didn\'t match expected type '\n        );\n"
+                "\n        \$this->assertInternalType(\n            '',\n            ,"
+                . "\n            'Return didn\'t match expected type '\n        );\n"
             ),
             array('type2value', 'null')
         );
