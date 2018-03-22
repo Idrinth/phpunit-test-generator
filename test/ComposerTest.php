84fab46432743c31d411b2dc5171cc079e987791
Björn Büttner <Idrinth@users.noreply.github.com>
2018-03-22 23:44:35 +0100

    tests for phpunit testcase class checks

--- test/ComposerTest.php
+++ test/ComposerTest.php
@@ -56,4 +56,51 @@ class ComposerTest extends TestCaseImplementation
             'Return didn\'t match expected array'
         );
     }
+
+    /**
+     * @return array
+     */
+    public function provideGetTestClass()
+    {
+        $old = 'PHPUnit_Framework_TestCase';
+        $new = 'PHPUnit\Framework\TestCase';
+        return array(
+            array('^1', false, $old),
+            array('^2', false, $old),
+            array('^3', false, $old),
+            array('^4', false, $new),
+            array('^5', false, $new),
+            array('^6', false, $new),
+            array('^7', false, $new),
+            array('^3|^4|^5|^6', true),
+            array('^1|^7', true),
+            array('4.5.6|^7', true),
+            array('^4|^7', false, $new),
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider provideGetTestClass
+     * @param string $constraint
+     * @param boolean $willThrow
+     * @param string $return
+     * @throws \ReflectionException
+     */
+    public function testGetTestClass($constraint, $willThrow, $return = '')
+    {
+        if ($willThrow) {
+            $this->setExpectedException(
+                'InvalidArgumentException',
+                'No possibility to determine PHPunit TestCase class found'
+            );
+        }
+        $instance = new Composer(new SplFileInfo(dirname(__DIR__).DIRECTORY_SEPARATOR.'composer.json'));
+        $class = new \ReflectionClass($instance);
+        $method = $class->getMethod('findTestClass');
+        $result = $method->invoke($instance, $constraint);
+        if (!$willThrow) {
+            $this->assertEquals($return, $result);
+        }
+    }
 }
