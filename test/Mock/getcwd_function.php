cf4581b83943f87b6088f1dce8f84f5bb1a709d8
Björn Büttner <Idrinth@users.noreply.github.com>
2018-04-09 15:37:35 +0200

    adding missing phpdoc

--- test/Mock/getcwd_function.php
+++ test/Mock/getcwd_function.php
@@ -3,6 +3,10 @@ namespace De\Idrinth\TestGenerator;
 
 use De\Idrinth\TestGenerator\Test\Mock\GetCwd;
 
+/**
+ * faked work dir returned
+ * @return string
+ */
 function getcwd()
 {
     return GetCwd::getcwd();
